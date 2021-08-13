dd5f237ed341796ff38b35c0cea24ffcdf39d0fe
jmhar <jmhar@protonmail.com>
2021-08-13 17:32:13 +1000

    simple component

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,23 @@
 // Code SimpleComponent Here
+import {Component} from 'react';
+
+export default class SimpleComponent extends Component {
+    constructor() {
+        super()
+        state = {
+            mood: happy
+        }
+    }
+
+    handleClick() {
+        this.setState = {
+            mood: sad
+        }
+    }
+
+    render() {
+        return(
+            <div onClick={handleClick}></div>
+        )
+    }
+}
